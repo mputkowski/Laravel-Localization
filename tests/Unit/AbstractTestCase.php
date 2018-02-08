271ca25ab6b4b95c8c77f5dbcb399033f05cfe35
mputkowski <m.putkowski@gmail.com>
2018-02-08 18:23:41 +0000

    Apply fixes from StyleCI

--- tests/Unit/AbstractTestCase.php
+++ tests/Unit/AbstractTestCase.php
@@ -1,9 +1,10 @@
 <?php
+
 namespace mputkowski\Tests\Locale\Unit;
 
+use GrahamCampbell\TestBench\AbstractPackageTestCase;
 use mputkowski\Locale\Locale;
 use mputkowski\Locale\LocaleServiceProvider;
-use GrahamCampbell\TestBench\AbstractPackageTestCase;
 
 abstract class AbstractTestCase extends AbstractPackageTestCase
 {
@@ -14,7 +15,7 @@ abstract class AbstractTestCase extends AbstractPackageTestCase
 
     protected function getLocale(array $config = [])
     {
-        $default_config = include(__DIR__ . '/../../config/locale.php');
+        $default_config = include __DIR__.'/../../config/locale.php';
         $config = array_replace_recursive($default_config, $config);
         $config['default_locale'] = 'en';
 
